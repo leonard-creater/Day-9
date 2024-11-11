ae10e6996f76c6841e4fa017bccb47fad015dcf7
leonard-creater <smileme12369@gmail.com>
2024-11-11 11:37:38 +0000

    my self

---
+++ My self.py
@@ -0,0 +1,13 @@
+class students:
+    name="Leonard"
+    grade=12
+
+    def intro(self):
+        print("I go by th name",self.name)
+    def details(self):
+        print("I'am in grade",self.grade)
+        print("I am a student of Ubas basic School",self.name)
+
+ob=students()
+ob.intro()
+ob.details()
